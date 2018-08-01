5d7bdf671e3b1aece427dbaf416c7229db87a243
Daniyar Myrzakanov <Daniyar.Myrzakanov@kcell.kz>
2018-08-01 11:24:38 +0600

    Servlet. Servlet Interface

---
+++ src/First.java
@@ -0,0 +1,35 @@
+import java.io.*;
+import javax.servlet.*;
+
+public class First implements Servlet{
+    ServletConfig config = null;
+    @Override
+    public void init(ServletConfig servletConfig) throws ServletException {
+        this.config=config;
+        System.out.println("servlet is initialized");
+    }
+
+    @Override
+    public ServletConfig getServletConfig() {
+        return config;
+    }
+
+    @Override
+    public void service(ServletRequest servletRequest, ServletResponse servletResponse) throws ServletException, IOException {
+        servletResponse.setContentType("text/html");
+        PrintWriter out = servletResponse.getWriter();
+        out.print("<b>Hello simple servlet</b>");
+        out.print(getServletInfo());
+        out.print(getServletConfig());
+    }
+
+    @Override
+    public String getServletInfo() {
+        return "copyright 2018";
+    }
+
+    @Override
+    public void destroy() {
+        System.out.println("servlet is destroyed");
+    }
+}
---
+++ web/index.jsp
@@ -0,0 +1,16 @@
+<%--
+  Created by IntelliJ IDEA.
+  User: mdaniyar
+  Date: 8/1/18
+  Time: 11:11 AM
+  To change this template use File | Settings | File Templates.
+--%>
+<%@ page contentType="text/html;charset=UTF-8" language="java" %>
+<html>
+  <head>
+    <title>$Title$</title>
+  </head>
+  <body>
+  <a href="hello">Invoke Generic Servlet</a>
+  </body>
+</html>
