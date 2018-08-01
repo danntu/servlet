a255453330305cd1f0fd08a6a2e23734edb63aa1
Daniyar Myrzakanov <Daniyar.Myrzakanov@kcell.kz>
2018-08-01 14:28:35 +0600

    Servlet. Request method getHeaderNames

--- src/DemoServ.java
+++ src/DemoServ.java
@@ -4,6 +4,7 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
 import java.io.PrintWriter;
+import java.util.Enumeration;
 
 public class DemoServ extends HttpServlet {
     @Override
@@ -12,6 +13,14 @@ public class DemoServ extends HttpServlet {
         resp.setCharacterEncoding("UTF-8");
         PrintWriter writer =  resp.getWriter();
         writer.print("Welcome " +req.getParameter("name"));
+        writer.println("HTTP headers sent by your client:<br>");
+        Enumeration<String> enumeration = req.getHeaderNames();
+        while (enumeration.hasMoreElements()) {
+            String headerName = (String) enumeration.nextElement();
+            String headerValue = req.getHeader(headerName);
+            writer.print("<b>"+headerName + "</b>: ");
+            writer.println(headerValue + "<br>");
+        }
         writer.close();
     }
 }
