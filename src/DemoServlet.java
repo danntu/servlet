c9e3f68cda98891993cd76d4508f27283ded9b74
Daniyar Myrzakanov <Daniyar.Myrzakanov@kcell.kz>
2018-08-01 16:13:18 +0600

    Servlet. Interface ServletConfig method getInitParameterNames()

--- src/DemoServlet.java
+++ src/DemoServlet.java
@@ -4,6 +4,7 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
 import java.io.PrintWriter;
+import java.util.Enumeration;
 
 public class DemoServlet extends HttpServlet {
     @Override
@@ -11,11 +12,21 @@ public class DemoServlet extends HttpServlet {
         resp.setContentType("text/html");
         PrintWriter out = resp.getWriter();
         out.print("Welcome to DemoServlet<br>");
-        out.println("<b>Using init-param web.xml Driver is"+getServletConfig().getInitParameter("driver")+"</b>");
+        out.print("<b>Using init-param web.xml Driver is"+getServletConfig().getInitParameter("driver")+"</b><br>");
+
+        out.print(" getInitParameterNames");
+        Enumeration<String> e =getServletConfig().getInitParameterNames();
+        String str="";
+        while(e.hasMoreElements()){
+            str=e.nextElement();
+            out.print("<br>Name: "+str);
+            out.print(" value: "+getServletConfig().getInitParameter(str));
+        }
+
         try {
             Thread.sleep(1000);
-        } catch (InterruptedException e) {
-            e.printStackTrace();
+        } catch (InterruptedException ex) {
+            ex.printStackTrace();
         }
         //resp.sendRedirect("http://google.kz");
         out.close();
